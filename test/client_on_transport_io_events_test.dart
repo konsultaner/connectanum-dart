89bac0255d2a05666198dd537dd73c010320a765
Yurii Prykhodko <yurii.prykhodko@solid.software>
2023-09-14 19:04:35 +0300

    Remove unused import

--- test/client_on_transport_io_events_test.dart
+++ test/client_on_transport_io_events_test.dart
@@ -13,7 +13,6 @@ import 'package:connectanum/src/transport/socket/socket_helper.dart';
 import 'package:connectanum/src/transport/socket/socket_transport.dart';
 import 'package:connectanum/src/transport/websocket/websocket_transport_io.dart';
 import 'package:connectanum/src/transport/websocket/websocket_transport_serialization.dart';
-import 'package:logging/logging.dart';
 import 'package:test/test.dart';
 
 void main() {
